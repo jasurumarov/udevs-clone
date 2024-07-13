6b1c883ccf2a9e4cb35acf037e3c25f9daeb0f16
jasurumarov <jasurumarov199@gmail.com>
2024-07-14 01:30:12 +0500

    head changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import '@/scss/style.scss'
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <title>IT-Аутсорсинг компания Udevs - IT консалтинг, разработка и внедрение ERP систем, разработка мобильных приложений, UI / UX дизайн, оптимизация инфраструктуры</title>
+      </head>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
